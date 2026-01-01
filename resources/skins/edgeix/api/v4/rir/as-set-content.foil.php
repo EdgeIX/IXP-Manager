15e26c0803e4d710d9c3a303d3934ad4899f1101
root <joe@edgeix.net>
2026-01-01 16:24:15 +0800

    updating the work on the edgeix skin from prod

---
+++ resources/skins/edgeix/api/v4/rir/as-set-content.foil.php
@@ -0,0 +1,15 @@
+password: <?= config('ixp_api.rir.password') ?>
+
+
+as-set:         AS24305:AS-EDGEIX-NET
+descr:          ASNs connected to the EdgeIX L3 Network
+descr:          EdgeIX operates multiple IXPs across Australia
+admin-c:        EPLA20-AP
+tech-c:         EPLA20-AP
+notify:         notify@edgeix.net
+remarks:        EdgeIX peer ASNs 
+mnt-by:         MAINT-EDGEIXPTYLTD-AU
+members:	AS24305
+members:        AS24224:AS-EDGEIX-SYD-RS
+source:         APNIC
+
--- resources/skins/edgeix/api/v4/rir/as-set-sydney-rs.foil.php
+++ resources/skins/edgeix/api/v4/rir/as-set-sydney-rs.foil.php
@@ -1,14 +1,14 @@
 password: <?= config('ixp_api.rir.password') ?>
 
 
-as-set:         AS-SET-EDGEIX-SYD-RS
+as-set:         AS24224:AS-EDGEIX-SYD-RS
 descr:          ASNs connected to the Route Servers at EdgeIX Sydney
 descr:          EdgeIX operates multiple IXPs across Australia
-admin-c:        TCMJ1-AP
-tech-c:         TCMJ1-AP
+admin-c:        EPLA20-AP
+tech-c:         EPLA20-AP
 notify:         notify@edgeix.net
 remarks:        EdgeIX peer ASNs are listed in AS-SET-EDGEIX-CONNECTED
-mnt-by:         MAINT-TCMJ-AU
+mnt-by:         MAINT-EDGEIXPTYLTD-AU
 <?php foreach( $t->customers as $c ): ?>
 <?php if( $c->routeServerClient( ) && $c->hasVLAN(200) ): ?>
 members:        <?= $c->asMacro( 4, 'AS' ) ?>
