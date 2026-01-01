074f07e0426808fe7c4136e866ffb200d1bfade7
Jason Playne <jason@jasonplayne.com>
2026-01-01 15:10:13 +0800

    edgeix port across Asn hackery

--- app/Console/Commands/Irrdb/UpdateAsnDb.php
+++ app/Console/Commands/Irrdb/UpdateAsnDb.php
@@ -73,7 +73,7 @@ class UpdateAsnDb extends UpdateDb
 
         foreach( $customers as $c ) {
             try {
-                $task = new UpdateAsnDbTask( $c );
+                $task = new UpdateAsnDbTask( $c, [ 4 ] );
                 $this->printResults( $c, $task->update(), 'asn' );
             } catch( Exception $e ) {
                 $this->handleException( $c, $e, 'ASN' );
