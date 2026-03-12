967b239ad6a796edd18f69f0164e1f7bf7d8ec62
Joe Wooller <joe@slampt.net>
2026-03-12 16:23:37 +0800

    I hate images

--- resources/skins/edgeix/layouts/ixp-logo-header.foil.php
+++ resources/skins/edgeix/layouts/ixp-logo-header.foil.php
@@ -1,4 +1,4 @@
 <a class="navbar-brand d-flex align-items-center" href="<?= url('') ?>" style="gap: 0.6rem; padding: 0.1rem 0;">
-    <img src="<?= asset( "images/EdgeIX-Mark-Brand.png" ) ?>" alt="EdgeIX" style="height: 36px; width: auto; mix-blend-mode: multiply;">
+    <img src="<?= asset( "images/EdgeIX-Mark-Brand.png" ) ?>" alt="EdgeIX" style="height: 36px; width: auto; mix-blend-mode: screen;">
     <span style="font-size: 1.15rem; font-weight: 700; letter-spacing: -0.02em; color: #FFFFFF;"><?= config('identity.titlename' ) ?></span>
 </a>
