e8ee116839f0585c18bc2b64bd168bd3cf12ec9e
Joe Wooller <joe@slampt.net>
2026-03-14 10:17:14 +0800

    Colours

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if(file_exists(public_path('images/EdgeIX-Mark-Brand.png')))
-<img src="cid:brand-logo" alt="{{ $slot }}" height="50" style="height: 50px; width: auto; vertical-align: middle; margin-right: 8px;">
+<img src="cid:brand-logo" alt="EdgeIX" height="50" style="height: 50px; width: auto; vertical-align: middle; margin-right: 8px;">
 @endif
-<span style="vertical-align: middle;">{{ $slot }}</span>
+<span style="vertical-align: middle; color: #CC7A2F; font-size: 22px; font-weight: bold;">EdgeIX</span>
 </a>
 </td>
 </tr>
