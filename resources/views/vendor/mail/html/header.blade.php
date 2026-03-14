ca8ca721911050844f5effaf8ac9c3a9669df908
Joe Wooller <joe@slampt.net>
2026-03-14 09:19:37 +0800

    images go!

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if(file_exists(public_path('images/EdgeIX-Mark-Brand.png')))
-<img src="{{ asset('images/EdgeIX-Mark-Brand.png') }}" alt="{{ $slot }}" height="50" style="height: 50px; width: auto; vertical-align: middle; margin-right: 8px;">
+<img src="{{ rtrim($url, '/') }}/images/EdgeIX-Mark-Brand.png" alt="{{ $slot }}" height="50" style="height: 50px; width: auto; vertical-align: middle; margin-right: 8px;">
 @endif
 <span style="vertical-align: middle;">{{ $slot }}</span>
 </a>
