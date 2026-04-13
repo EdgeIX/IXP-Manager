806b0b61ee1f4d3ad751288aa78f8c951ec47554
Joe Wooller <joe@slampt.net>
2026-04-13 11:25:16 +0800

    Remove CORE links from ports overview tab, shouldn't be here.

--- resources/skins/edgeix/customer/overview-tabs/ports.foil.php
+++ resources/skins/edgeix/customer/overview-tabs/ports.foil.php
@@ -11,6 +11,12 @@
     $servicePorts = [];
 
     foreach( $t->c->virtualInterfaces as $vi ) {
+        // Skip core uplinks, monitor ports, and management ports
+        $viType = $vi->type();
+        if( in_array( $viType, [ \IXP\Models\SwitchPort::TYPE_CORE, \IXP\Models\SwitchPort::TYPE_MONITOR, \IXP\Models\SwitchPort::TYPE_MANAGEMENT ], true ) ) {
+            continue;
+        }
+
         if( $vi->vlanInterfaces->isEmpty() ) {
             $servicePorts[] = $vi;
         } else {
