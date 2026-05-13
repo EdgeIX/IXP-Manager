f9840c5960a5e667ae32caa4a99d209b355fe9bf
Joe Wooller <joe@slampt.net>
2026-05-13 14:53:29 +0800

    UI Changes

--- resources/skins/edgeix/layouts/staff-links.foil.php
+++ resources/skins/edgeix/layouts/staff-links.foil.php
@@ -1,10 +1,12 @@
-<?php // Override this file (via skinning) to add customer staff links for ADMINs ?>
+<?php // EdgeIX-specific admin links (superuser only — included from menus/superuser.foil.php) ?>
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle center-dd-caret d-flex" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Staff Link
+<li class="nav-item dropdown <?= !request()->is( 'mac-sync*' ) ?: 'active' ?>">
+    <a class="nav-link dropdown-toggle center-dd-caret d-flex" href="#" id="navbarDropdownStaffLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        EdgeIX
     </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-<!--        <a class="dropdown-item" href="--><?//= route('xero.auth.success') ?><!--"></a>-->
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownStaffLink">
+        <a class="dropdown-item <?= !request()->is( 'mac-sync*' ) ?: 'active' ?>" href="<?= route( 'mac-sync@index' ) ?>">
+            MAC Sync
+        </a>
     </div>
 </li>
